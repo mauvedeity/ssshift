05aa38e4226e0d86f670cdc4c7880f5bd13c7156
mauvedeity <me@mauvedeity.com>
2026-07-15 19:19:29 +0100

    Stab 1 at productionising

--- ssshift.py
+++ ssshift.py
@@ -5,10 +5,22 @@
 # Use boto3 to move files into s3
 #
 import boto3
-s3 = boto3.client('s3')
-s3.upload_file(
-  Filename = 'data.dat',
-  Bucket= 'mauvedeity-backup',
-  Key = 'folder/data.dat'
-)
+import os
 
+def mmmoveit(mfile):
+  s3 = boto3.client('s3')
+  remote = 'pds/' + os.path.basename(mfile)
+  s3.upload_file(
+    Filename = mfile,
+    Bucket= 'mauvedeity-backup',
+    Key = remote
+  )
+
+def process(args):  # first one is name of script, second one is name of file to ssshift
+  if len(args) <> 2:
+    exit(1)
+  else:
+    mmmoveit(args[1])
+
+if __name__ == '__main__':
+  process(sys.argv)
